3b6c5d0f1c5ca598ad257ec73ad8908a2755027e
sandeep <formycore@gmail.com>
2026-05-05 22:44:09 +0530

    added terraform file for ec2

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+# 1. Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" # Change to your preferred region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "sandeep-my-terraform-demo-bucket-123456"
+# 2. Define the EC2 Instance
+resource "aws_instance" "example_server" {
+  ami           = "ami-07a00cf47dbbc844c" # ubuntu 20.04 LTS in ap-south-1
+  instance_type = "t3.micro"             # Free-tier eligible type
+
+  tags = {
+    Name = "TerraformExampleInstance"
+  }
 }
